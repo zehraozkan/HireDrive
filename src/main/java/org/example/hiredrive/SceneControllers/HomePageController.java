162600b1a5bf1f1bd3b0ebac8ed8c72e19c4a80b
zehraozkan <tayyibe.ozkan@ug.bilkent.edu.tr>
2024-05-07 12:25:45 +0300

    homepage update

--- src/main/java/org/example/hiredrive/SceneControllers/HomePageController.java
+++ src/main/java/org/example/hiredrive/SceneControllers/HomePageController.java
@@ -2,10 +2,14 @@ package org.example.hiredrive;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import org.example.hiredrive.Connection.UserConnection;
 
 public class HomePageController {
+
 
     @FXML
     private CheckBox CompanyCheck;
@@ -14,7 +18,10 @@ public class HomePageController {
     private CheckBox DriverCheck;
 
     @FXML
-    private TextArea resultArea;
+    private Label resultLabel;
+
+    @FXML
+    private Button getButton;
 
     @FXML
     void Checked(ActionEvent event) {
@@ -22,5 +29,10 @@ public class HomePageController {
         if(event.getSource() == DriverCheck){
             System.out.println("driver checked");
         }
+    }
+    @FXML
+    void buttonClicked(ActionEvent event) {
+
+        resultLabel.setText(UserConnection.getAllUsers());
     }
 }
