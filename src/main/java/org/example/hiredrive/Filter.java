0e4166eee60e88b256014008497ba04ca54b1ee6
Derin Ceylin Atalar <145395895+DerinCeylinAtalar@users.noreply.github.com>
2024-05-10 19:35:34 +0300

    other methods will be completed

---
+++ src/main/java/org/example/hiredrive/Filter.java
@@ -0,0 +1,36 @@
+package org.example.hiredrive;
+
+import java.sql.Date;
+
+public class Filter {
+    private int jobID;
+    private String location;
+    private String description;
+    private String industry;
+    private String requirements;
+    private Enum experienceLevel;
+    private Date deadline;
+    private boolean isActive;
+
+    public Filter(int jobID, String description, Date deadline, boolean isActive){
+        this.jobID = jobID;
+        this.description = description;
+        this.deadline = deadline;
+        this.isActive = isActive;
+    }
+
+    private void updateListing(){
+      //to be done
+    }
+
+    private void closeListing(){
+      //to be done
+    }
+
+    private void setFilterCriteria(String requirements, String industry, String location, Enum experienceLevel){
+        this.location = location;
+        this.industry = industry;
+        this.requirements = requirements;
+        this.experienceLevel = experienceLevel;
+    }
+}
