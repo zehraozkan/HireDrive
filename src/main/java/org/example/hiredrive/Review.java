cb17dbc8013f794acf4132310ab1529fbc872cef
Derin Ceylin Atalar <145395895+DerinCeylinAtalar@users.noreply.github.com>
2024-05-10 18:32:29 +0300

    Review classi

---
+++ src/main/java/org/example/hiredrive/Review.java
@@ -0,0 +1,43 @@
+package org.example.hiredrive;
+
+import org.example.hiredrive.Connection.UserConnection;
+
+public class Review {
+    private int reviewID;
+    private User reviewer;
+    private String comment;
+    private int rating;
+
+    public Review(int reviewerID, String comment, int rating){
+        setReviewID();
+        this.reviewer = UserConnection.retrieveUser(reviewerID);
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    //getter methods
+    public int getReviewID(){
+        return this.reviewID;
+    }
+
+    public User getReviewer(){
+        return this.reviewer;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
+    public int getRating(){
+        return this.rating;
+    }
+
+    //setter methods
+    public void setRating(int rate){
+        this.rating = rate;
+    }
+
+    public void setReviewID(){
+        this.reviewID = (int) (Math.random() * 900000) + 100000;
+    }
+}
