ec8eb70237b8596f15d20cd1600147355cc1d6d3
Derin Ceylin Atalar <ceylin.atalar@ug.bilkent.edu.tr>
2024-05-13 19:46:32 +0300

    update

--- src/main/java/org/example/hiredrive/users/Company.java
+++ src/main/java/org/example/hiredrive/users/Company.java
@@ -15,7 +15,7 @@ public class Company extends User {
         super(username,  password, email, phoneNo);
 
         //adds the user to the database
-        UserConnection.addUser(username,"", email, password,phoneNo, "company", Date.valueOf(LocalDate.now()));//no surname for company
+        UserConnection.addUser(username, "", email, password, phoneNo, "company", Date.valueOf(LocalDate.now()), 0);//no surname for company
         userId = UserConnection.getUserID(email);
         worksWith = UserConnection.getAssociatedDrivers(userId);
         userType = "company";
