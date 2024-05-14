59e87488a0befa4992f3044ead10fedc422c3e2d
Kağan <kagan.yazici@ug.bilkent.edu.tr>
2024-05-14 21:10:52 +0300

    chatpage

---
+++ src/main/java/org/example/hiredrive/SceneControllers/ChatPageController.java
@@ -0,0 +1,61 @@
+package org.example.hiredrive.SceneControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Circle;
+
+public class ChatPageController extends SuperSceneController {
+
+    @FXML
+    private Button buttonSend1;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button main_btn;
+
+    @FXML
+    private TextArea messageBox1;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private Circle profilePicCircle;
+
+    @FXML
+    private Circle profilePicCircle1;
+
+    @FXML
+    private Circle profilePicCircle11;
+
+    @FXML
+    private Button searchByNameButton;
+
+    @FXML
+    private TextField searchByNameTextArea;
+
+    @FXML
+    private Button view_profile_btn;
+
+    @FXML
+    void btn_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMethod(KeyEvent event) {
+
+    }
+
+}
