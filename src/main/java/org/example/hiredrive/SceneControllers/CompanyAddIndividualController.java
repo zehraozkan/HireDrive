94124e804bec2c3d17213d2210661c3120f04cd6
Derin Ceylin Atalar <ceylin.atalar@ug.bilkent.edu.tr>
2024-05-15 00:15:20 +0300

    update

--- src/main/java/org/example/hiredrive/SceneControllers/CompanyAddIndividualController.java
+++ src/main/java/org/example/hiredrive/SceneControllers/CompanyAddIndividualController.java
@@ -8,11 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+import javafx.scene.shape.Shape;
+
 
 public class CompanyAddIndividualController extends SuperSceneController{
 
@@ -40,12 +39,6 @@ public class CompanyAddIndividualController extends SuperSceneController{
     @FXML
     private Button rating_btn;
 
-    private String date;
-    private String id;
-    private String companyName;
-    private String namesurname;
-    private String experience;
-
     @FXML
     void btn_clicked(ActionEvent event) {
             if(event.getSource() == rating_btn){
@@ -54,8 +47,44 @@ public class CompanyAddIndividualController extends SuperSceneController{
        
     }
 
-    private void setNewRatingImage() {
-    }
+    private double rating;
 
-   
+    private void setNewRatingImage() {
+        if(rating == 1){
+            Image im = new Image("/org/example/hiredrive/pngs/1.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 1.5){
+            Image im = new Image("/org/example/hiredrive/pngs/1.5.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 2){
+            Image im = new Image("/org/example/hiredrive/pngs/2.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 2.5){
+            Image im = new Image("/org/example/hiredrive/pngs/2.5.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 3){
+            Image im = new Image("/org/example/hiredrive/pngs/3.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 3.5){
+            Image im = new Image("/org/example/hiredrive/pngs/3.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 4){
+            Image im = new Image("/org/example/hiredrive/pngs/4.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 4.5){
+            Image im = new Image("/org/example/hiredrive/pngs/4.5.png",false);
+            ratingPng.setImage(im);
+        }
+        else if(rating == 5){
+            Image im = new Image("/org/example/hiredrive/pngs/5.png",false);
+            ratingPng.setImage(im);
+        }
+    }  
 }
