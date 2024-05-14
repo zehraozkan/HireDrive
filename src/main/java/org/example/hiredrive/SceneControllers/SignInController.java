ff6545c0c588fbd1ea6f0ddacf62481859fdf8ef
Kağan <kagan.yazici@ug.bilkent.edu.tr>
2024-05-15 02:34:27 +0300

    null pointer exception in driver chat page to home page fixed

--- src/main/java/org/example/hiredrive/SceneControllers/SignInController.java
+++ src/main/java/org/example/hiredrive/SceneControllers/SignInController.java
@@ -29,7 +29,7 @@ public class SignInController extends SuperSceneController{
     @FXML
     private Label wrongPrompt;
 
-    private User user;
+    public static User user;
 
     @FXML
     void LinkClicked(ActionEvent event) {
