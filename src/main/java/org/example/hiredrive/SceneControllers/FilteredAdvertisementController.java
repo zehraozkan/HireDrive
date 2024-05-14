6febbe2bb32b976a1023c164f864666fa03a4adf
zehraozkan <tayyibe.ozkan@ug.bilkent.edu.tr>
2024-05-15 02:46:39 +0300

    driver search update

---
+++ src/main/java/org/example/hiredrive/SceneControllers/FilteredAdvertisementController.java
@@ -0,0 +1,38 @@
+package org.example.hiredrive.SceneControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+public class FilteredDriverController {
+
+    @FXML
+    private ChoiceBox<?> addOrderSelector;
+
+    @FXML
+    private Button goMainPageScene;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Button myProfileButton;
+
+    @FXML
+    private Circle profilePicCircle;
+
+    @FXML
+    private Button searchByNameButton;
+
+    @FXML
+    private TextField searchByNameTextArea;
+
+    @FXML
+    void btn_clicked(ActionEvent event) {
+
+    }
+
+}
